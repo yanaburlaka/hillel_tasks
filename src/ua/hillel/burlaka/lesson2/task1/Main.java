fe0d8aa91e2544cd1ce97124ac2a2d8a32ab3bc5
yanaburlaka <yanaburlaka@gmail.com>
2018-11-17 17:06:45 +0200

    1st task done

--- src/ua/hillel/burlaka/lesson2/task1/Main.java
+++ src/ua/hillel/burlaka/lesson2/task1/Main.java
@@ -1,4 +1,15 @@
 package ua.hillel.burlaka.lesson2.task1;
 
 public class Main {
+
+    public static void main(String[] args) {
+        int size = 10;
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 101);
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
 }
