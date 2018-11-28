aa79ee7464682e0a352e55f63b12c65fa0adf16e
Yana Burlaka <yburlaka@slice.com>
2018-11-28 20:50:37 +0200

    add separate method

--- src/ua/hillel/burlaka/lesson1/task1/Main.java
+++ src/ua/hillel/burlaka/lesson1/task1/Main.java
@@ -2,14 +2,19 @@ package ua.hillel.burlaka.lesson1.task1;
 
 public class Main {
 
+    public static void ShowString(String inputSting) {
+        System.out.println(inputSting);
+    }
+
     public static void main(String[] args) {
         String newLine = System.getProperty("line.separator");
-        System.out.println("***     ***" + newLine +
+        String name = "***     ***" + newLine +
                 " ***   *** " + newLine +
                 "  *** ***  " + newLine +
                 "   *****   " + newLine +
                 "    ***    " + newLine +
                 "    ***    " + newLine +
-                "    ***    ");
+                "    ***    ";
+        ShowString(name);
     }
 }
