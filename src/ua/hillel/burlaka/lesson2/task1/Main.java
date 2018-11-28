7609df8c007bec518294bad25bcc993c52722e15
Yana Burlaka <yburlaka@slice.com>
2018-11-28 20:59:13 +0200

    add separate method

--- src/ua/hillel/burlaka/lesson2/task1/Main.java
+++ src/ua/hillel/burlaka/lesson2/task1/Main.java
@@ -2,14 +2,18 @@ package ua.hillel.burlaka.lesson2.task1;
 
 public class Main {
 
+    public static void setAndSeeArray (int[] array, int limit) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 101);
+        }
+        for (int i = 0; i < limit; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
     public static void main(String[] args) {
         int size = 10;
         int[] array = new int[size];
-        for (int i = 0; i < size; i++) {
-            array[i] = (int) (Math.random() * 101);
-        }
-        for (int i = 0; i < 5; i++) {
-            System.out.print(array[i] + " ");
-        }
+        setAndSeeArray(array, 5);
     }
 }
